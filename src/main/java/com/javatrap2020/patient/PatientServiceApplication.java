8f5eb283c4eb1ace07f6e991dcfe989ef90d9641
Oleh Boiko <javatrap2020@gmail.com>
2022-02-14 13:23:29 +0200

    init

---
+++ src/main/java/com/javatrap2020/patient/PatientServiceApplication.java
@@ -0,0 +1,37 @@
+package com.javatrap2020.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@SpringBootApplication
+@EnableMongoRepositories
+@EnableScheduling
+@EnableJpaAuditing
+public class PatientServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+
+
+}
