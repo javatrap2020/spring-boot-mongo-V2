50e0a6bb44da610100764791f974809f2a665880
Oleh Boiko <javatrap2020@gmail.com>
2022-02-14 14:30:29 +0200

    add repository002

--- src/main/java/com/javatrap2020/patient/repository/PatientRepository.java
+++ src/main/java/com/javatrap2020/patient/repository/PatientRepository.java
@@ -3,9 +3,11 @@ package com.javatrap2020.patient.repository;
 import com.javatrap2020.patient.model.Patient;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PatientRepository  extends MongoRepository<Patient, Long> {
     Optional<Patient> finByEmail(@Param("email") String email);
 }
